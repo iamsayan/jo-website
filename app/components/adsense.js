d617a193d3cd0ecbc86d9f330f4edf604119ceff
Sayan Datta <sdcreativity12@gmail.com>
2024-02-24 18:44:20 +0530

    added adsense

--- app/components/adsense.js
+++ app/components/adsense.js
@@ -1,16 +1,19 @@
 'use client'
 
+import NoSsr from "@/app/utils/nossr";
 import { Adsense as GoogleAdsense } from "@ctrl/react-adsense";
 
 function Adsense(props) {
     return (
         <div className="text-center my-3">
-            <GoogleAdsense
-                client="ca-pub-2589621197191732"
-                style={{ display: "block" }}
-                className="ad"
-                {...props}
-            />
+            <NoSsr>
+                <GoogleAdsense
+                    client="ca-pub-2589621197191732"
+                    style={{ display: "block" }}
+                    className="ad"
+                    {...props}
+                />
+            </NoSsr>
         </div>
     );
 }
