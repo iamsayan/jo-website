459abe16ed928a0f40b631eb98304583916fcc42
Sayan Datta <sdcreativity12@gmail.com>
2024-03-15 09:44:56 +0530

    added fb sdk params

--- app/components/comments.js
+++ app/components/comments.js
@@ -8,8 +8,8 @@ const Comments = () => {
 
     return (
         <div className="mt-6">
-            <FacebookProvider appId={process.env.NEXT_PUBLIC_FB_APP_ID}>
-                <FBComments href={`${process.env.SITE_URL}${pathname}`} width="100%" orderBy="time" lazy={true} />
+            <FacebookProvider appId={process.env.NEXT_PUBLIC_FB_APP_ID} version="v19.0" autoLogAppEvents={true} xfbml={true} language="en_GB">
+                <FBComments href={`${process.env.SITE_URL}${pathname}`} width="100%" orderBy="reverse_time" />
             </FacebookProvider>
         </div>
     );
