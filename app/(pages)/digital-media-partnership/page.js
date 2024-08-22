faf719b947daddb56bebc4f58a0f92affa76e011
Sayan Datta <sdcreativity12@gmail.com>
2024-08-22 22:18:22 +0530

    added dmp page

--- app/(pages)/digital-media-partnership/page.js
+++ app/(pages)/digital-media-partnership/page.js
@@ -45,7 +45,7 @@ export default function Page() {
 
     return (
         <Layout title="Digital Media Partnership" jsonLd={jsonLd}>
-            <Section title="Know More About" description={ <>Digigtal Media <span className="text-yellow-500">Partnership</span></> }>
+            <Section title="Know More About Digital" description={ <>Media <span className="text-yellow-500">Partnership</span></> }>
                 <div className="flex flex-col gap-6 text-justify">
                     <p>Digital Media Partnership Programme is a collaboration program for Jagadhatri Puja Commitees
                         across Chandannagar Mankundu and Bhadreswar. In this partnership program, we promote the
@@ -69,7 +69,7 @@ export default function Page() {
                             <tr>
                                 <th>Services</th>
                                 <th>Basic Package</th>
-                                <th className="bg-yellow-100  relative">Premium Package <span className="ml-2 bg-red-500 text-white text-xs px-2 py-1">Trending</span></th>
+                                <th className="bg-yellow-100  relative">Premium Package <span className="ml-2 bg-red-500 text-white text-xs px-2 py-1">Best Choice</span></th>
                             </tr>
                             </thead>
                             <tbody>
@@ -81,41 +81,41 @@ export default function Page() {
                             </tr>
                             <tr>
                                 <th className="bg-gray-100">Reels Videos</th>
-                                <td>1 <br/><small>(up to 30 seconds)</small></td>
-                                <td className="bg-yellow-50">4 <br/><small>(any duration)</small></td>
+                                <td>1 <br/><small>(Up to 30 seconds)</small></td>
+                                <td className="bg-yellow-50">4 <br/><small>(Any duration)</small></td>
                             </tr>
                             <tr>
                                 <th className="bg-gray-100">Audio & Video Content</th>
                                 <td>❌</td>
-                                <td className="bg-yellow-50">✅</td>
+                                <td className="bg-yellow-50">✅<br/><small>(We will provide edited high quality content)</small></td>
                             </tr>
                             <tr>
-                                <th className="bg-gray-100">Live Videos (During Puja)</th>
+                            <th className="bg-gray-100">Live Videos (During Puja)</th>
                                 <td>1</td>
                                 <td className="bg-yellow-50">2</td>
                             </tr>
-                            <tr>
-                                <th className="bg-gray-100">Drone Shoot</th>
-                                <td>❌</td>
-                                <td className="bg-yellow-50">✅</td>
-                            </tr>
+                            {/*<tr>*/}
+                            {/*    <th className="bg-gray-100">Drone Shoot</th>*/}
+                            {/*    <td>❌</td>*/}
+                            {/*    <td className="bg-yellow-50">✅</td>*/}
+                            {/*</tr>*/}
                             <tr>
                                 <th className="bg-gray-100">360° Virtual Tours</th>
                                 <td>❌</td>
-                                <td className="bg-yellow-50">✅</td>
+                                <td className="bg-yellow-50">✅<br/><small>(On open pandel)</small></td>
                             </tr>
                             <tr>
-                                <th className="bg-gray-100">Price</th>
-                                <td><del>₹800</del> <span className="text-xl ml-2">₹500</span></td>
-                                <td className="bg-yellow-50"><del>₹9500</del><span className="text-xl ml-2">₹5000</span><br/><small>(You save ₹4500)</small></td>
+                                <th className="bg-gray-100">Package Price</th>
+                                <td><del>₹799</del><span className="text-xl ml-2 font-bold">₹499</span></td>
+                                <td className="bg-yellow-50"><del>₹9499</del><span className="text-xl ml-2 font-bold">₹4999</span><br/><small>(You save ₹4500)</small></td>
                             </tr>
                             <tr>
                                 <th className="bg-gray-100">Click to Register</th>
                                 <td>
-                                    <RazorpayButton buttonId="pl_Ony68U6SyoZEI6"/>
+                                    <a data-disable-nprogress={true} href="https://razorpay.com/payment-button/pl_Ony68U6SyoZEI6/view" className="btn bg-yellow-500 border-2 border-yellow-500 py-2 px-4 h-auto min-h-full rounded-md hover:bg-transparent hover:border-yellow-500 hover:text-yellow-500">Register</a>
                                 </td>
                                 <td className="bg-yellow-50">
-                                    <RazorpayButton buttonId="pl_Ony68U6SyoZEI6" />
+                                    <a data-disable-nprogress={true} href="https://razorpay.com/payment-button/pl_Oo0wht5ntXU1gr/view" className="btn bg-yellow-500 border-2 border-yellow-500 py-2 px-4 h-auto min-h-full rounded-md hover:bg-transparent hover:border-yellow-500 hover:text-yellow-500">Register</a>
                                 </td>
                             </tr>
                             </tbody>
