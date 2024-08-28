062a21ac1c3bfcc91d5c950ec48a899f0bf43cc6
Sayan Datta <sdcreativity12@gmail.com>
2024-08-28 14:09:34 +0530

    converted into typescript

--- app/components/layout.jsx
+++ app/components/layout.jsx
@@ -1,13 +1,13 @@
-import React, { ReactNode } from 'react';
-import Main from '@/app/components/main';
+import Main from '@/app/components/main'
 import Breadcrumbs from "@/app/components/breadcrumbs";
 import { paytoneOne } from "@/app/fonts";
-import bg from '@/public/img.png';
 
-function Layout({ children, title, jsonLd, breadcrumbTitle, end, bgImg }) {
+import bg from '@/public/img.png'
+
+export default function Layout({ children, title, jsonLd = null, breadcrumbTitle = null, end = null, bgImg = null }) {
     return (
         <Main jsonLd={jsonLd}>
-            <div className="hero h-96" style={{ backgroundImage: `url(${bgImg ? bgImg.src : bg.src})` }}>
+            <div className="hero h-96" style={{backgroundImage: `url(${bgImg ? bgImg.src : bg.src})`}}>
                 <div className="hero-overlay bg-slate-900 bg-opacity-70"></div>
                 <div className="flex-col hero-content text-center text-white-content text-white p-0 pt-10">
                     <h1 className={`text-3xl ${paytoneOne.className}`}>{title}</h1>
@@ -16,7 +16,5 @@ function Layout({ children, title, jsonLd, breadcrumbTitle, end, bgImg }) {
             </div>
             {children}
         </Main>
-    );
-};
-
-export default Layout;
+    )
+}
