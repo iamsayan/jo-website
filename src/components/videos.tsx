ffcfcf7deb254affde482fa50fc9f0691c290de5
Sayan Datta <sdcreativity12@gmail.com>
2025-02-04 18:10:22 +0530

    Refactor Videos component: Simplify video state management and add height attribute to thumbnail images

--- src/components/videos.tsx
+++ src/components/videos.tsx
@@ -9,10 +9,7 @@ interface VideoInfo {
 }
 
 export default function Videos() {
-    const [video, setVideo] = useState<VideoInfo>({
-        id: '67g1r7Hin0E',
-        name: 'Best video of Jagadhatri Puja',
-    });
+    const [videoId, setVideoId] = useState<string>('67g1r7Hin0E');
 
     const videos: VideoInfo[] = [
         {
@@ -40,7 +37,7 @@ export default function Videos() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-4 gap-0 md:gap-5">
             <div className="col-span-3">
-                <YouTubeEmbed videoid={video.id} params="controls=0&playsinline=1&cc_load_policy=1&modestbranding=1&rel=0" />
+                <YouTubeEmbed videoid={videoId} params="controls=0&playsinline=1&cc_load_policy=1&modestbranding=1&rel=0" />
             </div>
             <div className="flex flex-col">
                 <div className="flex flex-row justify-between p-4 border-b-2">
@@ -49,11 +46,12 @@ export default function Videos() {
                 </div>
                 <div className="flex flex-col justify-between gap-2">
                     {videos.map((info, index) => (
-                        <div key={index} className="flex flex-row items-center gap-3 cursor-pointer" onClick={() => setVideo(info)}>
+                        <div key={index} className="flex flex-row items-center gap-3 cursor-pointer" onClick={() => setVideoId(info.id)}>
                             <div className="basis-1/4">
                                 <img
                                     className="pointer-events-none"
                                     width={300}
+                                    height={169}
                                     src={`https://i3.ytimg.com/vi/${info.id}/maxresdefault.jpg`}
                                     alt={info.name}
                                 />
